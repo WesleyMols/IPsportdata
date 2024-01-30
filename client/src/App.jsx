4b7969b8dd65822ad33ebc02fba048c57cfab9d1
Wesley <wmols@sogyo.nl>
2024-01-30 14:44:16 +0100

    did a gradle build

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,13 +1,16 @@
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
+import { FloatingInput } from "sogyo/wesley/ipsportdata/client/components/FloatingInput";
+import classNames from "classnames";
 import './App.css'
 
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
+  const [username, setUsername] = useState("");
   const onsubmit = async () => {
-    const result = await start(usename);
+    const result = await start(username);
 
   }
   return (
@@ -28,10 +31,24 @@ function App() {
         <p>
           Edit <code>src/App.jsx</code> and save to test HMR
         </p>
+        </div>
+        <div>
+
+        <form><ol>
+          <li>
+            <FloatingInput
+              id="username"
+              label="enter your name"
+              value={username}
+              onChange={e => setUsername(e.target.value)}
+              />
+          </li>
+          <li>
         <button onClick={() => onsubmit()}  >
           Analyse
           {console.log("clicked")}
         </button>
+        </li></ol></form>
       </div>
       <p className="read-the-docs">
         Click on the Vite and React logos to learn more
