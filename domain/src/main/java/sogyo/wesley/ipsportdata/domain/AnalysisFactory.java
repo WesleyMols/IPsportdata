00557199f988f5d77bc274d7467f7a7b2a5a1ba7
Wesley <wmols@sogyo.nl>
2024-02-02 13:50:58 +0100

    add hashcode override not used yet

--- domain/src/main/java/sogyo/wesley/ipsportdata/domain/AnalysisFactory.java
+++ domain/src/main/java/sogyo/wesley/ipsportdata/domain/AnalysisFactory.java
@@ -6,5 +6,16 @@ public class AnalysisFactory implements IFactory {
     public IAnalysor createNewAnalysis(String name) {
         return new Analysor(name);    
     }
-    
+
+   /*  @Override
+    public int hashCode() {
+        int result = 17;
+        String name = "Julia";
+        if (createNewAnalysis(name) != null) {
+            result = 31 * result + createNewAnalysis(name).hashCode();
+        }
+        return result;
+    }
+
+     */
 }
