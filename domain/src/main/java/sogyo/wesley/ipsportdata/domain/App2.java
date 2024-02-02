3e8b8a0e512cbbf5220386586f53880de4d25933
Wesley <wmols@sogyo.nl>
2024-02-02 13:25:02 +0100

    add unit tests

--- domain/src/main/java/sogyo/wesley/ipsportdata/domain/App2.java
+++ domain/src/main/java/sogyo/wesley/ipsportdata/domain/App2.java
@@ -11,8 +11,7 @@ public class App2 {
     }
 
     public static void main(String[] args) {
-        //System.out.println(new App2().getGreeting());
-
+  
          try{
             Class.forName("com.mysql.jdbc.Driver");
             System.out.println("null");
