998270717eb4bb58d9bcce8e065ca7254e3d2202
Wesley <wmols@sogyo.nl>
2024-02-02 13:51:50 +0100

    refactor

--- domain/src/test/java/sogyo/wesley/ipsportdata/domain/AnalysisFactoryTest.java
+++ domain/src/test/java/sogyo/wesley/ipsportdata/domain/AnalysisFactoryTest.java
@@ -1,6 +1,5 @@
 package sogyo.wesley.ipsportdata.domain;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertInstanceOf;
 
 import org.junit.jupiter.api.Test;
