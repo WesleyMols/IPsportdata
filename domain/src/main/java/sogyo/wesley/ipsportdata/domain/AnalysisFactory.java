8f942e944ecf6c18e93ad7b7beef6d09aebeb339
Wesley <wmols@sogyo.nl>
2024-02-05 15:50:44 +0100

    remove unused stuff

--- domain/src/main/java/sogyo/wesley/ipsportdata/domain/AnalysisFactory.java
+++ domain/src/main/java/sogyo/wesley/ipsportdata/domain/AnalysisFactory.java
@@ -7,15 +7,4 @@ public class AnalysisFactory implements IFactory {
         return new Analysor(name);    
     }
 
-   /*  @Override
-    public int hashCode() {
-        int result = 17;
-        String name = "Julia";
-        if (createNewAnalysis(name) != null) {
-            result = 31 * result + createNewAnalysis(name).hashCode();
-        }
-        return result;
-    }
-
-     */
-}
+  }
