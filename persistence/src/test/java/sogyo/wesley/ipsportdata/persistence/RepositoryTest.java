2cd1b8af7f1c5aa4e4e7c42640b6bc7ebd609681
Wesley <wmols@sogyo.nl>
2024-02-06 14:03:37 +0100

    readme

--- persistence/src/test/java/sogyo/wesley/ipsportdata/persistence/RepositoryTest.java
+++ persistence/src/test/java/sogyo/wesley/ipsportdata/persistence/RepositoryTest.java
@@ -1,5 +1,5 @@
 package sogyo.wesley.ipsportdata.persistence;
-import static org.junit.jupiter.api.Assertions.assertNotNull;
+
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import java.util.HashMap;
