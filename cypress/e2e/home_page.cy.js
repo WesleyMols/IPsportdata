225b45f41ba75a919baf00515d83117756003af7
Wesley <wmols@sogyo.nl>
2024-02-28 15:27:48 +0100

    add beforeeach for visit and context which is just a label

--- cypress/e2e/home_page.cy.js
+++ cypress/e2e/home_page.cy.js
@@ -1,6 +1,10 @@
 describe('The Home Page', () => {
-    it('successfully loads', () => {
-      cy.visit('/') // URL in cypress.config
+    beforeEach(() => {
+        cy.visit('/')
+    })
+
+    context("table selection", () => {
+        it('successfully loads', () => {
       cy.get('#username')
         .type('wes')
       cy.get('#weigth').type('60') // doesnot look like it matters if this value is a string or number
@@ -10,6 +14,17 @@ describe('The Home Page', () => {
       cy.get('#heart_rate').type(155)
       cy.get('button')
         .should('exist')
+        .contains('analyse')
         .click()
+    })
+    it('get table headers', () => {
+        cy.get('table > :nth-child(1) > :nth-child(3)')
+        cy.get('table')
+    })
+    })
+    
+    it('go to about page', () => {
+        cy.get('ul > :nth-child(2) > a').contains('About').click()
+        
     })
   })
